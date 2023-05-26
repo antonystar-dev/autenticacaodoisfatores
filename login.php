ad97bc432780db75ad036521c2c788612acec67d
antonystar-dev <antonioamuniz@outlook.com>
2023-05-26 02:36:28 -0300

    geração  e verificação do codigo funcionando

--- login.php
+++ login.php
@@ -1,5 +1,31 @@
 <?php
 
+$codigo = $_POST['codigo'];
+echo $codigo;
+echo '<a href="index.html">Voltar para pg anterior</a>';
+
+require './banco.php';
+$sql = 'SELECT codigo FROM verificador WHERE id=1';
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    //echo $row["codigo"];
+    //echo md5($row["codigo"]);
+    if (md5($codigo) == $row ["codigo"]) {
+        echo "vc será redirecionado para pg correta";
+        }else{
+            echo "esta errado";
+        }
+    
+  }
+} else {
+  echo "não carregou";
+}
+$conn->close();
+
+/*
 //header("Refresh:10");
 $teste = 'asdf';
 
@@ -13,7 +39,7 @@ if (md5($teste) === '912ec803b2ce49e4a541068d495ab570') {
     echo $aleatorio;
 
 
-/*
+
 for ($gerador = 100; $gerador <= 999; $gerador++) {
     echo md5($gerador),'<br>';
 }
--- paginadocodigo.php
+++ paginadocodigo.php
@@ -1,11 +1,11 @@
 <?php
 require './banco.php';
 //header("Refresh:3");
-$codigo= rand(10, 99);
+$codigo = rand(10, 99);
 echo $codigo;
 
 
-$sql = "UPDATE verificador SET verificador=$codigo WHERE id=1";
+$sql = "UPDATE verificador SET codigo=md5($codigo) WHERE id=1";
 
 if ($conn->query($sql) === TRUE) {
   echo "atualizado";
