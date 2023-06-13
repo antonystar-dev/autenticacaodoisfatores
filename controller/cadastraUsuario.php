3d0543ebd19dad9a56480a302e42af757b334129
antonystar-dev <antonioamuniz@outlook.com>
2023-06-13 03:33:59 -0300

    verifica se o email existe

--- controller/cadastraUsuario.php
+++ controller/cadastraUsuario.php
@@ -7,28 +7,43 @@ $nome = $_POST["nome"];
 $senha = $_POST["senha"];
 //$senha = md5($_POST["senha"]);
 $email = $_POST["email"];
-$perfil_id=2;
+$perfil_id = 2;
 
 
-$usuarioDTO = new UsuarioDTO();
-$usuarioDTO->setNome($nome);
-$usuarioDTO->setSenha($senha);
-$usuarioDTO->setEmail($email);
-$usuarioDTO->setPerfil_id($perfil_id);
+
+$usuarioDAO = new UsuarioDAO();
+$usuarios = $usuarioDAO->getAllUsuario();
+foreach ($usuarios as $u) {
+
+    $verificaEmail[] = $u["email"];
+}
+if (in_array($email, $verificaEmail)) {
+
+    $msg = "O e-mail informado ja existe na nossa base de dados.";
+    echo "<script>localStorage.setItem('mensagemNegativa', '$msg');</script>";
+    header("Refresh: 0;url='../'");
 
 
-$usuarioDAO = new UsuarioDAO;
-$sucesso = $usuarioDAO->salvarUsuario($usuarioDTO);
 
-if ($sucesso) {
-    $msg = "Cadastrado com sucesso";
-    echo "<script>";
-    
-    echo "window.location.href = '../?msg={$msg}';";
-    //echo "window.location.href = '../?msg={$msg}';";
-    echo "</script> ";
+
+} else {
+    $usuarioDTO = new UsuarioDTO();
+    $usuarioDTO->setNome($nome);
+    $usuarioDTO->setSenha($senha);
+    $usuarioDTO->setEmail($email);
+    $usuarioDTO->setPerfil_id($perfil_id);
+
+
+    $usuarioDAO = new UsuarioDAO;
+    $sucesso = $usuarioDAO->salvarUsuario($usuarioDTO);
+
+    if ($sucesso) {
+        $msg = "Usuario cadastrado com sucesso";
+        echo "<script>localStorage.setItem('mensagemPositiva', '$msg');
+        localStorage.setItem('estadoAtual', '3');
+        </script>";
+        header("Refresh: 0;url='../'");
+    }
 }
 
 ?>
-
-
